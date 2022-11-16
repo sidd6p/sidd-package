7fb12888b850c471f6a1a1d400b6812351ae1a6d
Siddhartha Purwar <siddpurwar@gmail.com>
2022-11-17 01:09:26 +0530

    Build: added setup.py
    setup.py for uplaoding the package to testpypi

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SiddP6",                     # This is the name of the package
+    version="0.0.1",                        # The initial release version
+    author="Siddhartha Purwar",                     # Full name of the author
+    description="Siddhartha's Resume Python Package",
+    long_description=long_description,      # Long description read from the the readme file
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      # Information to filter the project on PyPi website
+    python_requires='>=3.6',                # Minimum version requirement of the package
+    py_modules=["sidd"],             # Name of the python package
+    package_dir={'':'.'},     # Directory of the source code of the package
+    install_requires=[]                     # Install other dependencies if any
+)
