afe50c73dc19e5628778908a1bcb845915f8a3c6
Siddhartha Purwar <siddpurwar@gmail.com>
2022-12-17 21:22:59 +0530

    BUILT: experience module

--- sidd/__init__.py
+++ sidd/__init__.py
@@ -2,5 +2,6 @@ from .hello import *
 from .bye import *
 from .education import *
 from .project import *
+from .experience import *
 
 from .sidd import *
---
+++ sidd/experience/experience.py
@@ -0,0 +1,29 @@
+def experiences():
+    print(
+"""
+
++++++++++++++++++++++++++++++++++++++++EXPERIENCE+++++++++++++++++++++++++++++++++++++++
+
+*** TRADEMARKIA ***
+    • Project Manager + Product Manager Intern
+    • May 2022 – Jul 2022 | Remote
+    • Responsibility to make sure that all going projects accommodating 22 interns are in sync
+    • Designated as product managerfor 1 project that was on halt for 6 months
+
+*** POWSTIK ***
+    • Technical Manager + Software Engineering Intern 
+    • Mar 2022 – Aug 2022 | Vellore, India
+    • Managed the transaction process to the Cloud platform while dealing with a team of 9 people
+    • Facilitated the recruitment of technical team and organization building
+    • Built the flask-based back-end seller-portal and deployed using Azure function
+
+*** FUTURE READY TALENT INTERNSHIP ***
+    • Back-end Developer 
+    • Dec 2021 – Feb 2022 | Remote
+    • Built Fully functional e-commerce with real-time order status with 2 portal(seller buyer)
+    • Deployment in 2 Azure VM with the use of 1 Azure SQL and 1 Azure Blob
+    • Link: https://github.com/SIDD6P/OFFLINE-E-COMMERCE
+
+"""
+    )
+
