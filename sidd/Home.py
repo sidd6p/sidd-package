6300f5f1098018aac6f4826bac1d3169b2fd9db7
Siddhartha Purwar <siddpurwar@gmail.com>
2024-11-10 20:45:01 +0530

    BUILD: Added resume page

--- sidd/Home.py
+++ sidd/Home.py
@@ -10,6 +10,6 @@ experience_page = st.Page("./Experiences.py", title="Experiences")
 resume_page = st.Page("./Resume.py", title="Resume")
 
 
-pg = st.navigation([projects_page, link_page, experience_page])
+pg = st.navigation([projects_page, link_page, experience_page, resume_page])
 # st.set_page_config(page_title="Siddhartha", page_icon=":material/edit:")
 pg.run()
